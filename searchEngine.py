6049322b5860792827dd29c093ecf42e909b3066
lioraft <lioraftabi@gmail.com>
2024-02-24 00:19:09 +0200

    pushing project to github repo - initialized inverted index with tf-idf and page rank

---
+++ searchEngine.py
@@ -0,0 +1,4 @@
+'''
+This file is the search engine for the search_frontend.py file. It is responsible for
+the backend of the search engine and it's logic. the functions will be called by the frontend.
+'''
